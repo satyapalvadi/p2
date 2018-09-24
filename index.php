e9088eb99960c027d1322a20e9ad86b461ec593a
Satya Palvadi <satyapalvadi@gmail.com>
2018-09-24 00:46:39 -0400

    project structure

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>P2 - Calories Burned Calculator</title>
+    <meta charset='utf-8'>
+</head>
+<body>
+    <h1>Calories Burned Calculator</h1>
+</body>
+
+</html>
